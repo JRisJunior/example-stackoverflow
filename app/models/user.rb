8014d665810c26d70f1d9cb69977d411d94f93c5
JRisJunior <jeffmrubin@gmail.com>
2022-12-19 11:28:54 -0600

    add has_many and belongs_to relationships across app

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :user_answers
+  has_many :answers, through: :user_answers
+  has_many :user_questions
+  has_many :questions, through: :user_questions
 end
